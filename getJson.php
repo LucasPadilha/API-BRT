477a6c6033cd2c212e8185e598272b35e4e3fb04
Lucas Padilha <padilhaght@gmail.com>
2016-10-20 20:17:24 -0200

    adicionadas opções de informações a serem exibidas

--- getJson.php
+++ getJson.php
@@ -1,7 +1,9 @@
 <?php 
   include_once('autoload.php');
+  
   if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     header('Content-Type: text/json');
+
   	if ($_GET['alvo'] == 'onibus') {
 	    $parser = new API\UrlParser("http://webapibrt.rio.rj.gov.br/api/v1/brt");
 	    echo $parser->createCurl()->getWebpage();
